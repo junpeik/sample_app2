bb8e4da45673bf89823ca62c4bfb520d32f30e64
Junpei <kusumoto.0513.jp@gmail.com>
2015-07-23 12:05:45 +0900

    5.1まで

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,6 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+
+# Bootstrapの画像(glyphicons-halflings.pngなど)をプリコンパイル
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
