fe6c1caceeb6f626c7449f57f21cbaaceb883256
Junpei <kusumoto.0513.jp@gmail.com>
2015-07-29 20:33:42 +0900

    9章終了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     #authenticate #=> passwordが有効なときはユーザを返し、無効なときはfalseを返す。
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,10 +4,6 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
-    self.current_user = user
-  end
-
-  def current_user=(user)
     @current_user = user
   end
 
@@ -17,12 +13,25 @@ module SessionsHelper
     @current_user ||= User.find_by(remember_token: remember_token)
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def signed_in?
     !current_user.nil?
   end
 
   def sign_out
-    self.current_user = nil
+    @current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def store_location
+    session[:return_to] = request.url
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
   end
 end
